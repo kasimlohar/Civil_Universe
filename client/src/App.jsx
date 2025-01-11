4e52ced13a1b55043711b46e1c11890a6c8b51e7
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 15:43:12 +0530

    Add routing for Home, Login, Register, and BusinessProfile pages in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import Profile from './components/Profile';
-import Booking from './components/Booking';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import BusinessProfile from './pages/BusinessProfile';
 import './styles/App.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/booking" component={Booking} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/business-profile" component={BusinessProfile} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/business-profile" component={BusinessProfile} />
+      </Switch>
     </Router>
   );
-}
+};
 
 export default App;

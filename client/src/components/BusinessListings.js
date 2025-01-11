2163848ae053f0334b6fcd6d944049df8dc77cc9
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 17:15:43 +0530

    Enhanced BusinessListings component with business categories and description

--- client/src/components/BusinessListings.js
+++ client/src/components/BusinessListings.js
@@ -4,6 +4,14 @@ const BusinessListings = () => {
     return (
         <div>
             <h1>Business Listings Page</h1>
+            <p>Explore our comprehensive directory of civil-related businesses. Find contractors, architects, interior designers, material suppliers, and more.</p>
+            <ul>
+                <li>Contractors</li>
+                <li>Architects</li>
+                <li>Interior Designers</li>
+                <li>Material Suppliers</li>
+                <li>Fabricators</li>
+            </ul>
         </div>
     );
 };

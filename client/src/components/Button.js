5b332b9f12132d3f82536aa305af8575acf8e166
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 17:24:54 +0530

    Enhanced Button component with additional styling and functionality

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ type, children, onClick }) => {
-  return <button type={type} onClick={onClick} className="button">{children}</button>;
+const Button = ({ type, children, onClick, className, disabled }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`button ${className}`}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 };
 
 Button.propTypes = {
   type: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  className: '',
+  disabled: false,
 };
 
 export default Button;

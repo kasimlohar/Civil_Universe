f7fa85fef4288e6d2763c1ada008b4963337dc66
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 17:28:53 +0530

    Enhanced App.js with additional routes and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import BusinessListings from './components/BusinessListings'; // Ensure this matches the actual filename
+import BusinessListings from './components/BusinessListings';
 import Profile from './components/Profile';
 import Booking from './components/Booking';
 import BusinessList from './components/BusinessList';
+import Navbar from './pages/Navbar';
+import Footer from './pages/Footer';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/business-listings" component={BusinessListings} /> {/* Ensure this matches the actual filename */}
+        <Route path="/business-listings" component={BusinessListings} />
         <Route path="/profile" component={Profile} />
         <Route path="/booking" component={Booking} />
         <Route path="/business-list" component={BusinessList} />
       </Switch>
+      <Footer />
     </Router>
   );
 }

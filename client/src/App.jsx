8b7bee6c742e459ade1c433c38276b79db35afc3
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 15:32:58 +0530

    Add routes for Login, Register, and BusinessProfile pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './components/Profile';
 import Booking from './components/Booking';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import BusinessProfile from './pages/BusinessProfile';
 import './styles/App.css';
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/profile" component={Profile} />
           <Route path="/booking" component={Booking} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/business-profile" component={BusinessProfile} />
         </Switch>
       </div>
     </Router>

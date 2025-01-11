75d2b1ec5408f16537531a97fbb3175a906bde70
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 12:14:37 +0530

    Set up routing for Home, Business Listings, Profile, and Booking pages using react-router-dom.

---
+++ src/components/BusinessListings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusinessListings = () => {
+    return (
+        <div>
+            <h1>Business Listings Page</h1>
+        </div>
+    );
+};
+
+export default BusinessListings;

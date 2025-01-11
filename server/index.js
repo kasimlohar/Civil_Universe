9c540e60699341fb2b4548152fb93576fba76f1a
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 18:16:08 +0530

    Set up basic Express server with CORS and body-parser middleware

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log('Server is running');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const businessesRoutes = require('./routes/businesses');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/businesses', businessesRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Civil Universe API');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

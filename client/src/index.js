a65dc292da9b8d14ea7bae93de1fc018925d8f1f
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 20:22:19 +0530

    Create index.js to render App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import store from './store';
-import './styles/index.css';
 import App from './App';
+import './styles/index.css';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );

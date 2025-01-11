66e89c44fddf62de862752924029c21964e57869
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 20:21:55 +0530

    Create user reducer for managing user state

---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true,
+      };
+    case 'LOGOUT_USER':
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;

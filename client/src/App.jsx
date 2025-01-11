304e049625c8b2675c559f3c60a7ee453021a3df
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 14:37:05 +0530

    Added Home.js component and updated README.md with project structure details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './components/Profile';
+import Booking from './components/Booking';
 import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the MERN Full Stack Project</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/booking" component={Booking} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

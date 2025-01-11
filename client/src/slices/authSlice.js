51cee0910e5f84461e53384882c9a7c442f8e9e1
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 17:21:29 +0530

    Enhanced authSlice with additional actions and state for user authentication

--- client/src/slices/authSlice.js
+++ client/src/slices/authSlice.js
@@ -5,15 +5,18 @@ const authSlice = createSlice({
   initialState: {
     user: null,
     token: null,
+    isAuthenticated: false,
   },
   reducers: {
     setUser: (state, action) => {
       state.user = action.payload.user;
       state.token = action.payload.token;
+      state.isAuthenticated = true;
     },
     clearUser: (state) => {
       state.user = null;
       state.token = null;
+      state.isAuthenticated = false;
     },
   },
 });

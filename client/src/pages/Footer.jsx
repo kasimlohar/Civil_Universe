89e94a3f2756a40e095bed31fefa01cb091367db
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 17:18:57 +0530

    Enhanced Footer component with additional information and links

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -1,10 +1,16 @@
-
 import React from 'react';
 
 function Footer() {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className="footer bg-blue-600 text-white p-4">
+      <div className="container mx-auto">
+        <p>&copy; 2023 Civil Universe. All rights reserved.</p>
+        <ul className="flex justify-center space-x-4">
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/privacy">Privacy Policy</a></li>
+        </ul>
+      </div>
     </footer>
   );
 }

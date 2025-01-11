b5deb395ca6cc2784d7b96567e9572cb7dce255d
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 18:42:30 +0530

    Integrate user routes in server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const connectDB = require('./db');
 const businessesRoutes = require('./routes/businesses');
+const usersRoutes = require('./routes/users');
+
+// Load environment variables
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 // Middleware
 app.use(cors());
@@ -12,6 +20,7 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/api/businesses', businessesRoutes);
+app.use('/api/users', usersRoutes);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Civil Universe API');

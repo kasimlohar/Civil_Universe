aafe6df742b07fcbb96f94d27a5a60d21286e7cf
kasimlohar <kasimlohar2@gmail.com>
2025-01-11 18:42:19 +0530

    Add user controller with register and login functions

---
+++ server/controllers/userController.js
@@ -0,0 +1,35 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+// Register a new user
+exports.registerUser = async (req, res) => {
+  const { name, email, password, phone } = req.body;
+  try {
+    const user = new User({ name, email, password, phone });
+    await user.save();
+    const token = jwt.sign({ user: { id: user.id } }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(400).json({ message: 'Error registering user' });
+  }
+};
+
+// Login user
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ user: { id: user.id } }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};

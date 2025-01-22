99007a36c86a321424dd1072ac6d545d81f5c031
kasimlohar <kasimlohar2@gmail.com>
2025-01-22 22:09:00 +0530

    test: Add Home component tests

--- client/src/tests/components/Home.test.js
+++ client/src/tests/components/Home.test.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from '../../store';
 import Home from '../../pages/Home';
 
+const renderWithProviders = (ui) => {
+  return render(
+    <Provider store={store}>
+      <BrowserRouter>{ui}</BrowserRouter>
+    </Provider>
+  );
+};
+
 describe('Home Component', () => {
-  const renderHome = () => {
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <Home />
-        </BrowserRouter>
-      </Provider>
-    );
-  };
-
   test('renders main sections', () => {
-    renderHome();
-    expect(screen.getByText(/Find Trusted/i)).toBeInTheDocument();
+    renderWithProviders(<Home />);
     expect(screen.getByText(/Featured Businesses/i)).toBeInTheDocument();
-  });
-
-  test('search functionality works', () => {
-    renderHome();
-    const searchInput = screen.getByPlaceholderText(/Search for services/i);
-    fireEvent.change(searchInput, { target: { value: 'construction' } });
-    expect(searchInput.value).toBe('construction');
   });
 });

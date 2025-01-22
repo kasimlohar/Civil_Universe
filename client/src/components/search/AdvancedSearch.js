d832c1605121546d454f685ae151e8ff8c3c9fde
kasimlohar <kasimlohar2@gmail.com>
2025-01-22 22:37:37 +0530

    feat: Add advanced search functionality

--- client/src/components/search/AdvancedSearch.js
+++ client/src/components/search/AdvancedSearch.js
@@ -1,15 +1,16 @@
 import React, { useState, useCallback } from 'react';
-import debounce from 'lodash/debounce';
+import { debounce } from 'lodash';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { FaSearch, FaFilter } from 'react-icons/fa';
+import { BUSINESS_CATEGORIES } from '../../utils/constants';
 
 const AdvancedSearch = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [filters, setFilters] = useState({
-    query: searchParams.get('q') || '',
-    category: searchParams.get('category') || '',
-    location: searchParams.get('location') || '',
-    priceRange: searchParams.get('price') || ''
+    category: '',
+    priceRange: '',
+    rating: 0,
+    location: ''
   });
 
   const debouncedSearch = useCallback(

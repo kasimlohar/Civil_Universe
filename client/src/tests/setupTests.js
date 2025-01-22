6fdf3e6dbe729b6fe22efbdd272c1a160d292933
kasimlohar <kasimlohar2@gmail.com>
2025-01-22 22:08:52 +0530

    chore: Update testing setup and dependencies

--- client/src/tests/setupTests.js
+++ client/src/tests/setupTests.js
@@ -1,15 +1,30 @@
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
+
+global.matchMedia = global.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: jest.fn(),
+    removeListener: jest.fn()
+  };
+};
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => jest.fn()
+}));
+
+// Mock the socket.io-client
+jest.mock('socket.io-client', () => {
+  return jest.fn(() => ({
+    on: jest.fn(),
+    emit: jest.fn(),
+    disconnect: jest.fn()
+  }));
+});
+
 import { server } from './mocks/server';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
-
-global.matchMedia = global.matchMedia || function() {
-  return {
-    matches: false,
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn()
-  };
-};
